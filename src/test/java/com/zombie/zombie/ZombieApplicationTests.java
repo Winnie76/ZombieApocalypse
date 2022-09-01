c689c46feec8a6d281f795537631c8bf872d5e68
Yaoni <y.wang7@uq.net.au>
2022-09-01 18:28:06 +1000

    Init.

---
+++ src/test/java/com/zombie/zombie/ZombieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zombie.zombie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZombieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
