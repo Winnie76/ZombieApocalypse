80995cb461ca876455344cd034ecfd5bd2634175
Yaoni <y.wang7@uq.net.au>
2022-09-01 19:33:48 +1000

    Overload Cell constructor to allow no GameObject list to be passed in.

--- src/main/java/com/zombie/zombie/model/Cell.java
+++ src/main/java/com/zombie/zombie/model/Cell.java
@@ -2,12 +2,24 @@ package com.zombie.zombie.model;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class Cell {
+    // [x, y]
     private List<Integer> pos;
     // game objects on cell
     private List<GameObject> GOOnCell;
+
+    public Cell(List<Integer> pos) {
+        this.pos = pos;
+        this.GOOnCell = new ArrayList<>();
+    }
+
+    public Cell(List<Integer> pos, List<GameObject> GOOnCell) {
+        this.pos = pos;
+        this.GOOnCell = GOOnCell;
+    }
 
 }
