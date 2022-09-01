0b62f16766fa185006591164bcd989c9eb667acb
Yaoni <y.wang7@uq.net.au>
2022-09-01 19:34:32 +1000

    Override Cell equals and hashcode.

--- src/main/java/com/zombie/zombie/model/Cell.java
+++ src/main/java/com/zombie/zombie/model/Cell.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Data
 public class Cell {
@@ -22,4 +23,16 @@ public class Cell {
         this.GOOnCell = GOOnCell;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cell cell = (Cell) o;
+        return pos.equals(cell.pos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pos);
+    }
 }
