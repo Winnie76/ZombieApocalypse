21ea646349c97b076ca9d47435e3a7aa5ba20e2d
Yaoni <y.wang7@uq.net.au>
2022-09-03 20:37:58 +1000

    Add Unit Tests for Move.

--- src/test/java/com/zombie/zombie/service/MoveTest.java
+++ src/test/java/com/zombie/zombie/service/MoveTest.java
@@ -1,16 +1,48 @@
 package com.zombie.zombie.service;
 
+import com.zombie.zombie.model.Board;
+import com.zombie.zombie.model.GameCharacter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MoveTest {
+
+    GameCharacter zombie;
+    List<GameCharacter> creatures;
 
     @BeforeEach
     void setUp() {
-
+        zombie = new GameCharacter(0, 0, true);
     }
 
     @Test
-    void move() {
+    void noMove() {
+        Board.gridSize = 1;
+        GameCharacter.action = "";
+        Move.move(zombie);
+        assertEquals(0, zombie.x);
+        assertEquals(0, zombie.y);
+    }
+
+    @Test
+    void moveGridOne() {
+        Board.gridSize = 1;
+        GameCharacter.action = "U";
+        Move.move(zombie);
+        assertEquals(0, zombie.x);
+        assertEquals(0, zombie.y);
+    }
+
+    @Test
+    void moveGridMultiple() {
+        Board.gridSize = 4;
+        GameCharacter.action = "D";
+        Move.move(zombie);
+        assertEquals(0, zombie.x);
+        assertEquals(1, zombie.y);
     }
 }
