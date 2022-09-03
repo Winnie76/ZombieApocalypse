638d59090547e72bf82f6c96a9c85bdc642bbcc0
Yaoni <y.wang7@uq.net.au>
2022-09-03 20:17:26 +1000

    refactor controller.

--- src/main/java/com/zombie/zombie/controller/ZombieController.java
+++ src/main/java/com/zombie/zombie/controller/ZombieController.java
@@ -2,17 +2,15 @@ package com.zombie.zombie.controller;
 
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
-import com.zombie.zombie.model.Cell;
 import com.zombie.zombie.model.Character;
 import com.zombie.zombie.service.Move;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import static com.zombie.zombie.model.Board.gridSize;
@@ -23,7 +21,10 @@ import static com.zombie.zombie.service.Move.*;
 public class ZombieController {
 
     @PostMapping("/getValue")
-    public Map getValue(String value) {
+    @ResponseStatus(HttpStatus.CREATED)
+    @ExceptionHandler(Exception.class)
+    public HashMap<Object, Object> getValue(@RequestBody String value) {
+        System.out.println(value);
         initValue(value);
         Move.move(zombie);
         HashMap<Object, Object> map = new HashMap<>();
@@ -37,8 +38,8 @@ public class ZombieController {
     private void initValue(String value) {
         JSONObject jsonObject = JSON.parseObject(value);
         gridSize = Integer.parseInt(jsonObject.get("gridSize").toString());
-        zombie = JSON.parseObject(jsonObject.get("zombie").toString(), Cell.Zombie.class);
-        Cell.Creature[] creatureArr = JSON.parseObject(jsonObject.get("creatures").toString(), Cell.Creature[].class);
+        zombie = JSON.parseObject(jsonObject.get("zombie").toString(), Character.class);
+        Character[] creatureArr = JSON.parseObject(jsonObject.get("creatures").toString(), Character[].class);
         creatures = Arrays.stream(creatureArr).collect(Collectors.toList());
         Character.action = jsonObject.get("commands").toString();
         newZombies = new ArrayList<>();
