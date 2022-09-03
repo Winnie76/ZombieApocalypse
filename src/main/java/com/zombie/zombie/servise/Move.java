85fd0b8e190997e9a2753e27bb6adcc04c48cdb8
Winnie76 <biyaoc@student.unimelb.edu.au>
2022-09-03 18:31:32 +1000

    merge overWall code to move()

--- src/main/java/com/zombie/zombie/servise/Move.java
+++ src/main/java/com/zombie/zombie/servise/Move.java
@@ -21,41 +21,36 @@ public class Move {
     public static void moving(Cell.Zombie zombie) {
         String directions = GameObject.action.toUpperCase();
         char[] direction = directions.toCharArray();
+        Map<Integer, Integer> overWallPos = new HashMap<>(){{
+            put(gridSize, 0);
+            put(-1, gridSize - 1);
+        }};
         for (char ch : direction) {
             if (ch == 'R') {
                 zombie.x = (zombie.x - 1);
-                overWall(zombie);
-                isHasCreatures(zombie);
             }
             if (ch == 'L') {
                 zombie.x = (zombie.x + 1);
-                overWall(zombie);
-                isHasCreatures(zombie);
             }
             if (ch == 'D') {
                 zombie.y = (zombie.y - 1);
-                overWall(zombie);
-                isHasCreatures(zombie);
             }
             if (ch == 'U') {
                 zombie.y = (zombie.y + 1);
-                overWall(zombie);
-                isHasCreatures(zombie);
             }
+            
+            if (zombie.x >= gridSize || zombie.x < 0) {
+                zombie.x = overWallPos.get(zombie.x);
+            }else if (zombie.y >= gridSize || zombie.y < 0) {
+                zombie.y = overWallPos.get(zombie.y);
+            }
+
+            isHasCreatures(zombie);
         }
         newZombies.add(zombie);
 
     }
 
-    private static void overWall(Cell.Zombie zombie) {
-        Map<Integer, Integer> overWallPos = new HashMap<>(){{
-            put(gridSize, 0);
-            put(-1, gridSize - 1);
-        }};
-        zombie.x = overWallPos.get(zombie.x);
-        zombie.y = overWallPos.get(zombie.y);
-
-    }
 
     private static void isHasCreatures(Cell.Zombie zombie) {
         for (Cell.Creature creature : creatures) {
