89e657ddae9a237153502ef8a0bd9e693da9ed36
Yaoni <y.wang7@uq.net.au>
2022-09-06 17:16:34 +1000

    Refactor Move logic.

--- src/main/java/com/zombie/zombie/service/Move.java
+++ src/main/java/com/zombie/zombie/service/Move.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.*;
 
-import static com.zombie.zombie.model.Board.gridSize;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +19,7 @@ public class Move {
     public static List<GameCharacter> infectedCreatures = new ArrayList<>();
     public static List<GameCharacter> creatures = new ArrayList<>();
     public static GameCharacter zombie;
+    int gridSize;
     private static final Mapper mapper = new Mapper();
 
     Logger logger = LoggerFactory.getLogger(Logger.class);
@@ -27,13 +27,10 @@ public class Move {
     public GameResultDto move(GameConfigDto gameConfigDto) {
 
         logger.warn("LOGGING:" + gameConfigDto.toString());
+        gridSize = gameConfigDto.getGridSize();
         String commands = gameConfigDto.getCommands();// GameCharacter.action.toUpperCase();
         GameCharacter zombie = mapper.toZombie(gameConfigDto.getZombie());
         char[] directions = commands.toCharArray();
-        Map<Integer, Integer> overWallPos = new HashMap<>() {{
-            put(gridSize, 0);
-            put(-1, gridSize - 1);
-        }};
         Map<Character, List<Integer>> action = new HashMap<>() {{
             put('R', Arrays.asList(1, 0));
             put('L', Arrays.asList(-1, 0));
@@ -42,21 +39,14 @@ public class Move {
         }};
 
         for (char ch : directions) {
-            zombie.x = action.get(ch).get(0) + zombie.x;
-            zombie.y = action.get(ch).get(1) + zombie.y;
-
-            if (zombie.x >= gridSize || zombie.x < 0) {
-                zombie.x = overWallPos.get(zombie.x);
-            } else if (zombie.y >= gridSize || zombie.y < 0) {
-                zombie.y = overWallPos.get(zombie.y);
-            }
-
+            zombie.x = (action.get(ch).get(0) + gridSize + zombie.x) % gridSize;
+            zombie.y = (action.get(ch).get(0) + gridSize + zombie.y) % gridSize;
             infectOthers(zombie);
         }
         newZombies.add(zombie);
         return new GameResultDto(newZombies.stream()
-                .map(z -> mapper.toZombieDto(z)).toList(),
-                creatures.stream().map(c -> mapper.toCreatureDto(c)).toList());
+                .map(mapper::toZombieDto).toList(),
+                creatures.stream().map(mapper::toCreatureDto).toList());
     }
 
 
@@ -65,7 +55,6 @@ public class Move {
             if (creature.x == zombie.x && creature.y == zombie.y && !creature.isInfected()) {
                 creature.setInfected(true);
                 infectedCreatures.add(creature);
-                // this.move(new GameCharacter(zombie.x, zombie.y, true));
             }
         }
     }
