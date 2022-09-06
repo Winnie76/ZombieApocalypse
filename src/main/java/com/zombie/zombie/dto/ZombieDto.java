73cc7bccb8eaefc6baf55c4e1c577e18774b0232
Yaoni <y.wang7@uq.net.au>
2022-09-06 16:11:25 +1000

    Add AllArgsConstructor annotation.

--- src/main/java/com/zombie/zombie/dto/ZombieDto.java
+++ src/main/java/com/zombie/zombie/dto/ZombieDto.java
@@ -1,11 +1,13 @@
 package com.zombie.zombie.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@AllArgsConstructor
 public class ZombieDto {
     @NotBlank
     int x;
